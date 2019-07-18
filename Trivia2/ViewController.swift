99cce0a25d9059b6b7d475f18b164f55bc0244e6
Praveena <praveena@spuriq.com>
2019-07-18 13:21:29 -0700

    Next button implemented

--- Trivia2/ViewController.swift
+++ Trivia2/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var questions = ["What has holes but can hold water",
+                     "What has three feet but no toes",
+                     "What is tall when it is young and short when it is old"]
+    var currQn = 0
+    var answers = ["Sponge", "Yard stick", "Candle"]
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var userAnswerTextField: UITextField!
+    @IBOutlet weak var validationLabel: UILabel!
+    @IBOutlet weak var endOfGameMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currQn]
     }
 
-
+    @IBAction func checkButtonClick(_ sender: Any) {
+        if(userAnswerTextField.text == ""){
+            validationLabel.text = "Please enter an answer."
+            return
+        }
+        let userAnswer = userAnswerTextField.text
+        let correctAnswer = answers[currQn]
+        
+        if(userAnswer!.caseInsensitiveCompare(correctAnswer) == .orderedSame) {
+            validationLabel.text = "Correct."
+        } else {
+            validationLabel.text = "Incorrect. Correct answer is \(correctAnswer)"
+        }
+        currQn += 1
+        if (currQn >= questions.count){
+            endOfGameMessageLabel.text = "Well done. Good game."
+        } else {
+            questionLabel.text = questions[currQn]
+        }
+        userAnswerTextField.text = ""
+    }
 }
 
